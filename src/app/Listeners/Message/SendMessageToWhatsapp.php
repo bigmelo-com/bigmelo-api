829c0d7acb8e23abae70a96f5cab80f2c6e251c3
Abel Moreno <moreno.abel@gmail.com>
2023-08-28 19:06:41 -0500

    6 - Updating ChatGPT events

--- src/app/Listeners/Message/SendMessageToWhatsapp.php
+++ src/app/Listeners/Message/SendMessageToWhatsapp.php
@@ -3,7 +3,7 @@
 namespace App\Listeners\Message;
 
 use App\Classes\Twilio\TwilioClient;
-use App\Events\Message\AdminMessageStored;
+use App\Events\Message\BigmeloMessageStored;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
@@ -13,26 +13,26 @@ class SendMessageToWhatsapp
     /**
      * Handle the event.
      */
-    public function handle(AdminMessageStored $event): void
+    public function handle(BigmeloMessageStored $event): void
     {
-        $admin_message = $event->message;
+        $bigmelo_message = $event->message;
 
         try {
-            $user = $admin_message->user;
+            $user = $bigmelo_message->user;
 
             $twilio_client = new TwilioClient(env('TWILIO_PHONE_NUMBER'));
 
-            $twilio_client->sendMessageToWhatsapp($user->full_phone_number, $admin_message->content);
+            $twilio_client->sendMessageToWhatsapp($user->full_phone_number, $bigmelo_message->content);
 
             Log::info(
                 'Message sent to whatsapp, ' .
-                'message_id: ' . $admin_message->id
+                'message_id: ' . $bigmelo_message->id
             );
 
         } catch (\Throwable $e) {
             Log::error(
                 'SendMessageToWhatsapp: Internal error, ' .
-                'admin_message_id: ' . $admin_message->id . ', ' .
+                'admin_message_id: ' . $bigmelo_message->id . ', ' .
                 'error: ' . $e->getMessage()
             );
         }
