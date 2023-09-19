ea276d10a44390c1cd66e3dbcd9ba18c311c820e
Abel Moreno <moreno.abel@gmail.com>
2023-09-19 16:39:22 -0500

    33 - New models and migrations to manage projects

---
+++ src/app/Models/Lead.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Lead extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'country_code',
+        'phone_number',
+        'full_phone_number',
+        'email',
+        'country',
+        'state',
+        'city',
+        'gender',
+        'birthdate',
+        'active'
+    ];
+
+    /**
+     * User related to the lead
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
---
+++ src/app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'organization_id',
+        'name',
+        'description',
+        'system_prompt',
+        'active'
+    ];
+
+    /**
+     * Organization linked to the project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class, 'organization_id', 'id');
+    }
+}
