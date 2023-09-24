8154e0e4d5467507d09e00941a6f1ac3906f0a15
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-09-24 14:44:38 -0500

    41 - Signup user endpoint and user creation test

--- src/app/Http/Controllers/api/v1/AuthController.php
+++ src/app/Http/Controllers/api/v1/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use OpenApi\Annotations as OA;
 
@@ -84,9 +86,45 @@ class AuthController extends Controller
             return response()->json(['message' => 'Your email or password are incorrect.'], 403);
         }
     }
-    public function signUp(Request $request): JsonResponse {
 
-        return response()->json(['message' => 'Invalid name'], 500);
-        
+    public function signUp(Request $request): JsonResponse 
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'last_name' => 'required|string',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+                'country_code' => 'required|string',
+                'phone_number' => 'required|string',
+                'full_phone_number' => 'required|string',
+            ]);
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->last_name = $request->last_name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->country_code = $request->country_code;
+            $user->phone_number = $request->phone_number;
+            $user->full_phone_number = $request->full_phone_number;
+            $user->role = 'user';
+            $user->save();
+
+            $token = $user->createToken('token-name', $user->getRoleAbilities());
+
+            return response()->json(
+                [
+                    'access_token' => $token->plainTextToken,
+                    'user' => $user,
+                ],
+                200
+            );
+
+        } catch (ValidationException $e) {
+            return response()->json(['message' => $e->errors()], 400);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 }
