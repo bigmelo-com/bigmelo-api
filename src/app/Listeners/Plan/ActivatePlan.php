4d831a767c10ea019bf717a9697f0ee2b2ffa338
Juan Acevedo <juan.acevedo9805@gmail.com>
2024-05-14 08:45:15 -0500

    110 - Add message and set up schedule command

--- src/app/Listeners/Plan/ActivatePlan.php
+++ src/app/Listeners/Plan/ActivatePlan.php
@@ -2,13 +2,16 @@
 
 namespace App\Listeners\Plan;
 
+use App\Events\Message\BigmeloMessageStored;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Repositories\MessageRepository;
+use Illuminate\Support\Facades\Log;
 use App\Events\Plan\PlanActivated;
+use Illuminate\Support\Facades\DB;
+use App\Models\Project;
 use App\Models\Lead;
 use App\Models\Plan;
 use Carbon\Carbon;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class ActivatePlan
 {
@@ -20,6 +23,9 @@ class ActivatePlan
     public function handle(PlanActivated $event): void
     {
         $transaction = $event->transaction;
+        $project = Project::find(1);
+        $message_repository = new MessageRepository();
+        
         try {
             $plan = Plan::find($transaction->plan_id);
             $lead = Lead::find($transaction->lead_id);
@@ -27,9 +33,20 @@ class ActivatePlan
             $lead->remaining_messages = $plan->message_limit;
             $lead->plan_start_at = Carbon::now();
             $lead->save();
-
+            
             $transaction->status = 'completed';
             $transaction->save();
+            
+            $twilio_plan_purchased_template = "Hola {$lead->first_name}! 🌟 ya puedes disfrutar de tu nuevo plan! ¿Sobre qué te gustaría conversar?";
+            
+            $message = $message_repository->storeMessage(
+                lead_id: $lead->id,
+                project_id: $project->id,
+                content: $twilio_plan_purchased_template,
+                source: 'Admin'
+            );
+
+            event(new BigmeloMessageStored($message));
 
             DB::table('lead_plan_logs')->insert([
                 'lead_id'           => $lead->id,
