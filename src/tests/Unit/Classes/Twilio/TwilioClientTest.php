a0f86b2569069b29a6b34ffa32cbde9a7ec4473c
Abel Moreno <abel@Abels-MacBook-Pro.local>
2024-05-30 18:32:57 -0500

    77 - Getting text from audio file

--- src/tests/Unit/Classes/Twilio/TwilioClientTest.php
+++ src/tests/Unit/Classes/Twilio/TwilioClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Classes\Twilio;
 
+use App\Classes\ChatGPT\ChatGPTClient;
 use App\Classes\Message\ChatGPTMessage;
 use App\Classes\Message\ChatGPTMessageResponse;
 use App\Classes\Twilio\TwilioClient;
@@ -10,6 +11,7 @@ use App\Models\User;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 /**
@@ -28,10 +30,19 @@ class TwilioClientTest extends TestCase
      */
     public function get_file_content(): void
     {
-        $file_url = 'https://api.twilio.com//Media/MEeed77c79931868d622e7d4774adc77e2';
-
+//        $file_url = 'https://api.twilio.com/Media/MEeed77c79931868d622e7d4774adc77e2';
+//
 //        $twilio_client = new TwilioClient('1');
 //        $file_content = $twilio_client->getFileContent($file_url);
+//
+//        $filename = 'audio_' . time() . '.ogg';
+//
+//        Storage::disk('public')->put($filename, $file_content);
+//
+//        $chatgpt_client = new ChatGPTClient();
+//        $response = $chatgpt_client->getTextFromAudioFile(storage_path('app/public/' . $filename));
+//
+//        print_r($response); die;
 
         $this->assertTrue(true);
     }
