1ffcc30168ab0b656d1ebd45ab23ce21f0f25b46
Juan Acevedo <juan.acevedo9805@gmail.com>
2024-06-21 14:06:31 -0500

    Update deploy script

--- scripts/start-queue.sh
+++ scripts/start-queue.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-cd /home/ubuntu/bigmelo-api
-git stash
-git stash drop
 cd /home/ubuntu/bigmelo-api/src/
 npm i
 npm audit fix
 composer install
 
 cd /home/ubuntu/bigmelo-api
-git stash
-git stash drop
 git fetch origin
 sudo git reset --hard origin/main
 git pull --rebase origin main
+
 cd /home/ubuntu/bigmelo-api/src/
 php artisan migrate --seed
 nohup php artisan queue:work --daemon >/dev/null 2>&1 &
